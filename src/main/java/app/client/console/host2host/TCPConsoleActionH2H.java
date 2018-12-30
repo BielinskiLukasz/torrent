202375bb82a81081db9feb2691f8154b9698a627
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2018-12-30 11:27:43 +0100

    Remove outdated method from host2host version (need update)

--- src/main/java/app/client/console/host2host/TCPConsoleActionH2H.java
+++ src/main/java/app/client/console/host2host/TCPConsoleActionH2H.java
@@ -7,11 +7,7 @@ import app.utils.Logger;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
@@ -25,9 +21,6 @@ public class TCPConsoleActionH2H {
         switch (ConsoleCommand.valueOf(command)) {
             case FILE_LIST:
                 getFileList(command, connectedHostPortNumber);
-                break;
-            case PULL:
-                pull(clientNumber, userSentence, connectedHostPortNumber); // TODO remove sourceClientNumber from message
                 break;
             case EMPTY_COMMAND:
                 break;
@@ -130,102 +123,5 @@ public class TCPConsoleActionH2H {
         }
 
         Logger.appLog("Server file list was displayed");
-    }
-
-    private static void pull(int clientNumber, String userSentence, int connectedHostPortNumber) {
-        Logger.appDebugLog("fire pull");
-
-        int sourceClientNumber = ActionUtils.getClientNumber(userSentence);
-
-        if (sourceClientNumber == clientNumber) {
-            Logger.appLog("It is not the customer you are looking for :)");
-            Logger.appLog("There is no need to download the file from yourself");
-        } else {
-
-            Socket connectionSocket = null;
-            try {
-                connectionSocket = new Socket(Config.HOST_IP, Config.PORT_NR + connectedHostPortNumber);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            DataOutputStream outToClient = null;
-            try {
-                outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            String command = ActionUtils.getCommand(userSentence);
-            String fileName = ActionUtils.getFileName(userSentence);
-
-            Logger.appDebugLog(command + " output: " + clientNumber + " " + fileName);
-            try {
-                outToClient.writeBytes(command + Config.SPLITS_CHAR + clientNumber +
-                        Config.SPLITS_CHAR + fileName + "\n");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            InputStream inputStream = null;
-            try {
-                inputStream = connectionSocket.getInputStream();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            BufferedReader inFromClient = null;
-            try {
-                inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            String response = null;
-            try {
-                response = inFromClient.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Logger.appDebugLog(command + " input: " + response);
-
-            Logger.appLog(response);
-
-            File file = new File(Config.BASIC_PATH + clientNumber + "//" + fileName);
-            FileOutputStream fileOutputStream = null;
-            try {
-                fileOutputStream = new FileOutputStream(file);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            Logger.appDebugLog(command + " open fileOutputStream");
-
-            int count;
-            byte[] buffer = new byte[8192];
-            try {
-                while ((count = inputStream.read(buffer)) > 0) {
-                    fileOutputStream.write(buffer, 0, count);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                fileOutputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Logger.appDebugLog(command + " close fileOutputStream");
-
-            // TODO implements checking md5 sum (and delete file if aren't correct) ==> UPDATE H2H dirs
-
-            try {
-                connectionSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            Logger.appLog("Finish sending file");
-        }
     }
 }
