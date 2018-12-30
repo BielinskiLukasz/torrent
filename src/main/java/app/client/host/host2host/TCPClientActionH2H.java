14790847a4f05e0295163f956382151cddf1f337
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2018-12-30 17:48:24 +0100

    Remove outdated method from host2host version (need update)

--- src/main/java/app/client/host/host2host/TCPClientActionH2H.java
+++ src/main/java/app/client/host/host2host/TCPClientActionH2H.java
@@ -8,12 +8,8 @@ import app.utils.Logger;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.util.List;
 
@@ -31,9 +27,6 @@ public class TCPClientActionH2H {
                 break;
             case FILE_LIST:
                 getFileList(client.getClientNumber(), connectionSocket, clientSentence);
-                break;
-            case PULL:
-                pull(client.getClientNumber(), connectionSocket, clientSentence); // TODO check it
                 break;
             default:
                 Logger.clientLog('"' + command + '"' + " command is not supported yet");
@@ -119,97 +112,5 @@ public class TCPClientActionH2H {
         );
 
         Logger.clientLog("Client file list sent to server");
-    }
-
-    private static void pull(int clientNumber, Socket connectionSocket, String clientSentence) {
-        Logger.clientDebugLog("fire pull");
-
-        String command = ActionUtils.getCommand(clientSentence);
-        int targetClientNumber = ActionUtils.getClientNumber(clientSentence);
-        String fileName = ActionUtils.getFileName(clientSentence);
-        String response;
-
-        OutputStream outputStream = null;
-        try {
-            outputStream = connectionSocket.getOutputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        DataOutputStream outToServer = null;
-        try {
-            outToServer = new DataOutputStream(connectionSocket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        File file = new File(Config.BASIC_PATH + clientNumber + "//" + fileName);
-        if (file.exists()) {
-
-            response = "Sending file " + fileName + " started";
-            try {
-                outToServer.writeBytes(response + "\n");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            FileInputStream fileInputStream = null;
-            try {
-                fileInputStream = new FileInputStream(file);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-
-            int count;
-            byte[] buffer = new byte[Config.BUFFER_SIZE_IN_BYTES];
-            try {
-                while ((count = fileInputStream.read(buffer)) > 0) {
-                    outputStream.write(buffer, 0, count);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                outputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                fileInputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            Logger.clientLog("Send file " + fileName + " to client " + targetClientNumber);
-
-        } else {
-
-            response = "Client " + clientNumber + " doesn't share file " + fileName +
-                    ". Chceck file name and client number";
-            try {
-                outToServer.writeBytes(response + "\n");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                byte[] buffer = new byte[Config.BUFFER_SIZE_IN_BYTES];
-                outputStream.write(buffer);
-            } catch (IOException e) {
-                System.out.println("Error: " + e);
-            }
-            Logger.clientDebugLog(command + " send fake file");
-
-            try {
-                outputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Logger.clientDebugLog(command + " close outputStream");
-        }
-
-        Logger.clientDebugLog(command + " sending sequence ended");
     }
 }
