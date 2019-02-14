de31a44f939a0af22af17a34dfaa3315a9604901
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2019-02-14 21:32:35 +0100

    Fix test script

--- src/test/testScripts/run_MH.bat
+++ src/test/testScripts/run_MH.bat
@@ -1,4 +1,4 @@
-start java -jar ..\..\..\out\artifacts\torrentHost_jar\torrentServer.jar
-start java -jar ..\..\..\out\artifacts\torrentHost_jar\torrentClient.jar 1
-start java -jar ..\..\..\out\artifacts\torrentHost_jar\torrentClient.jar 2
-start java -jar ..\..\..\out\artifacts\torrentHost_jar\torrentClient.jar 3
+start java -jar ..\..\..\out\artifacts\torrentServer_jar\torrentServer.jar
+start java -jar ..\..\..\out\artifacts\torrentClient_jar\torrentClient.jar 1
+start java -jar ..\..\..\out\artifacts\torrentClient_jar\torrentClient.jar 2
+start java -jar ..\..\..\out\artifacts\torrentClient_jar\torrentClient.jar 3
